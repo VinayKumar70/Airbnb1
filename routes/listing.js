3057ccf5ad8789c2143ac5fa81ff730993841b07
VinayKumar70 <215034768+VinayKumar70@users.noreply.github.com>
2026-09-15 16:32:21 +0530

    Production read Airbnb clone 6

--- routes/listing.js
+++ routes/listing.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync = require("../utils/wrapAsync.js");
-const listingController = require("../controller/listing.js")
-const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-const {storage} = require("../cloudConfig.js");
-const multer = require("multer");
-const upload = multer({storage});
 
+const wrapAsync = require("../utils/wrapAsync.js");
+const listingController = require("../controller/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const { storage } = require("../cloudConfig.js");
+const multer = require("multer");
+const upload = multer({ storage });
+
+// 1. Index & Create Route
 router.route("/")
   .get(wrapAsync(listingController.index))
   .post(
@@ -19,14 +21,16 @@ router.route("/")
     wrapAsync(listingController.createListing)
   );
 
+// 2. Form Route (HAMESHA :id SE PEHLE HONA CHAHIYE)
+router.get("/rent", isLoggedIn, listingController.renderRent);
 
-
+// 3. Show, Update & Delete Routes (:id HAMESHA AAKHRI ME)
 router.route("/:id")
-.get( wrapAsync(listingController.renderShow))
-.put( isLoggedIn, isOwner,upload.single("listing[image]"), validateListing, wrapAsync(listingController.edit))
-.delete( isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+  .get(wrapAsync(listingController.renderShow))
+  .put(isLoggedIn, isOwner, upload.single("listing[image]"), validateListing, wrapAsync(listingController.edit))
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
-//edit route
+// 4. Edit Route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEdit));
 
 module.exports = router;
